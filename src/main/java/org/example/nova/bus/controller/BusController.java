7b96ffc0b0e9ffe366d606e8391370cb9fd017a9
hgo641 <hgo641@hdc-labs.com>
2025-08-28 13:53:43 +0900

    fix: 버스 도착시간 조회 컨트롤러에 Get메서드 정보 추가 및 쿼리 파라미터 적용

--- src/main/java/org/example/nova/bus/controller/BusController.java
+++ src/main/java/org/example/nova/bus/controller/BusController.java
@@ -3,6 +3,7 @@ package org.example.nova.bus.controller;
 import org.example.nova.bus.dto.BusArrivalRequest;
 import org.example.nova.bus.dto.BusArrivalResponse;
 import org.example.nova.bus.service.BusService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/bus")
 public class BusController {
 
-//    private final BusService busService;
-//
-//    public BusController(BusService busService) {
-//        this.busService = busService;
-//    }
-//
-//    public BusArrivalResponse getBusArrival(@RequestBody BusArrivalRequest busArrivalRequest) {
-//        return busService.getBusArrival(busArrivalRequest);
-//    }
+    private final BusService busService;
+
+    public BusController(BusService busService) {
+        this.busService = busService;
+    }
+
+    @GetMapping("/arrival")
+    public BusArrivalResponse getBusArrival(BusArrivalRequest busArrivalRequest) {
+        return busService.getBusArrival(busArrivalRequest);
+    }
 }
