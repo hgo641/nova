a8a75400d18264dee4c9ae6548d24abd4bb7c624
hgo641 <hgo641@hdc-labs.com>
2025-12-16 15:36:28 +0900

    refactor: @Transactional 추가 및 JPARepository로 변경

--- src/main/java/org/example/nova/message/service/MessageService.java
+++ src/main/java/org/example/nova/message/service/MessageService.java
@@ -5,6 +5,7 @@ import org.example.nova.message.domain.MessageRepository;
 import org.example.nova.message.dto.MessageCreateRequest;
 import org.example.nova.message.dto.MessageResponse;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class MessageService {
@@ -15,14 +16,17 @@ public class MessageService {
         this.messageRepository = messageRepository;
     }
 
+    @Transactional(readOnly = true)
     public MessageResponse getMessage(Long id) {
-        Message message = messageRepository.findById(id);
+        Message message = messageRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("메시지가 존재하지 않습니다. id=" + id));
+
         return new MessageResponse(message.getMessage(), message.getCreatedAt());
     }
 
+    @Transactional
     public Long createMessage(MessageCreateRequest messageCreateRequest) {
-        Message message = new Message(messageCreateRequest.getMessage());
-        long messageId = messageRepository.createMessage(message);
-        return messageId;
+        Message savedMessage = messageRepository.save(new Message(messageCreateRequest.getMessage()));
+        return savedMessage.getId();
     }
 }
