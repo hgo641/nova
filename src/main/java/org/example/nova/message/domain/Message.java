e74a75e3a9ad137da425e4888bea4378a057cf92
hgo641 <hgo641@hdc-labs.com>
2025-12-16 15:35:48 +0900

    refactor: JPA Entity설정 추가 및 ID필드 추가

--- src/main/java/org/example/nova/message/domain/Message.java
+++ src/main/java/org/example/nova/message/domain/Message.java
@@ -1,14 +1,30 @@
 package org.example.nova.message.domain;
 
+import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
+@Entity
 public class Message {
-    private final String message;
-    private final LocalDateTime createdAt;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String message;
+
+    private LocalDateTime createdAt;
+
+    protected Message() {
+        // JPA 기본 생성자
+    }
 
     public Message(String message) {
         this.message = message;
         this.createdAt = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getMessage() {
