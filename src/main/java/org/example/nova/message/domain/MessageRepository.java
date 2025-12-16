4c233d39d41e9b21ae8290832a5806e8831f5676
hgo641 <hgo641@hdc-labs.com>
2025-12-16 15:24:25 +0900

    feat: JDBC 사용 Repository 추가

--- src/main/java/org/example/nova/message/domain/MessageRepository.java
+++ src/main/java/org/example/nova/message/domain/MessageRepository.java
@@ -2,29 +2,71 @@ package org.example.nova.message.domain;
 
 import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
-import java.util.Map;
+import javax.sql.DataSource;
+import java.sql.*;
 import java.util.NoSuchElementException;
 
 @Repository
 public class MessageRepository {
 
-    private static long id = 1;
-    private static final Map<Long, Message> messageRepository = new HashMap<>();
+    private final DataSource dataSource;
+
+    public MessageRepository(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     public Message findById(Long id) {
-        if (!messageRepository.containsKey(id)){
-            throw new NoSuchElementException("주어진 아이디에 해당하는 메시지가 존재하지 않습니다. 아이디 : " + id);
-        }
+        String sql = """
+                    SELECT id, message, created_at
+                    FROM message
+                    WHERE id = ?
+                """;
 
-        return messageRepository.get(id);
+        try (
+                Connection conn = dataSource.getConnection();
+                PreparedStatement ps = conn.prepareStatement(sql)
+        ) {
+            ps.setLong(1, id);
+
+            try (ResultSet rs = ps.executeQuery()) {
+                if (!rs.next()) {
+                    throw new NoSuchElementException("메시지가 존재하지 않습니다. id=" + id);
+                }
+
+                return new Message(
+                        rs.getLong("id"),
+                        rs.getString("message"),
+                        rs.getTimestamp("created_at").toLocalDateTime()
+                );
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public long createMessage(Message message) {
-        long messageId = id;
-        id = id + 1;
+        String sql = """
+                    INSERT INTO message (message)
+                    VALUES (?)
+                """;
 
-        messageRepository.put(messageId, message);
-        return messageId;
+        try (
+                Connection conn = dataSource.getConnection();
+                PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)
+        ) {
+            ps.setString(1, message.getMessage());
+            ps.executeUpdate();
+
+            try (ResultSet keys = ps.getGeneratedKeys()) {
+                if (keys.next()) {
+                    return keys.getLong(1);
+                }
+                throw new SQLException("ID 생성 실패");
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
